ed3c8dc9f0ac72f7a56764cd5de967ec052af83c
dannitay <dannielletaylor@outlook.com>
2021-02-23 23:26:18 -0600

    adding route to search for specific param

--- server.js
+++ server.js
@@ -42,7 +42,12 @@ function filterByQuery(query, animalsArray) {
     }
     // return the filtered results:
     return filteredResults;
-  }
+}
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -50,6 +55,15 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    }else {
+        res.send('invalid: This animal does not exist');
+    }
 });
 
 
