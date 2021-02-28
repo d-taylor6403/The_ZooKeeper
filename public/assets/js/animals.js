ba1ef022917ca0d3d8d01232ea9dff379761773c
d-taylor6403 <dannielletaylor@outlook.com>
2021-02-27 21:30:08 -0600

    adding fetch request for animals

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -31,6 +31,18 @@ const getAnimals = (formData = {}) => {
 
   console.log(queryUrl);
 
+  fetch(queryUrl)
+  .then(response => {
+    if (!response.ok) {
+      return alert('Error: ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(animalData => {
+    console.log(animalData);
+    printResults(animalData);
+  });
+
 };
 
 const handleGetAnimalsSubmit = event => {
